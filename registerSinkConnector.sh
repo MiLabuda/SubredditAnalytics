e034fa075388044124e73f29e7bd40bd14143c44
Michał Łabuda <mlabuda544@gmail.com>
2025-01-12 20:19:23 +0100

    Add registering kafka sink connector to postgres as a script

---
+++ registerSinkConnector.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+curl -X POST -H "Content-Type: application/json" \
+  --data '{
+    "name": "kafka-to-postgres-sink",
+    "config": {
+      "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
+      "tasks.max": "6",
+      "topics": "reddit-posts,reddit-comments",
+      "connection.url": "jdbc:postgresql://postgres:5432/subreddit-db",
+      "connection.user": "user",
+      "connection.password": "developmentpassword",
+      "insert.mode": "upsert",
+      "auto.create": "true",
+      "auto.evolve": "true",
+      "pk.mode": "record_value",
+      "pk.fields": "id"
+    }
+  }' \
+  http://localhost:8084/connectors
